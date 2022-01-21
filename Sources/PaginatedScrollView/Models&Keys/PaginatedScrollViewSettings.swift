6d57bd6224c1af309777542522d02c835d4f3c3a
Aung Ko Min <jonahaung@gmail.com>
2022-01-22 06:17:26 +0800

    1

--- Sources/PaginatedScrollView/Models&Keys/PaginatedScrollViewSettings.swift
+++ Sources/PaginatedScrollView/Models&Keys/PaginatedScrollViewSettings.swift
@@ -9,11 +9,11 @@ import UIKit
 
 public struct PaginatedScrollViewSettings {
     
-    public let moreLoaderTreshold: CGFloat
-    public let reloaderTreshold: CGFloat
-    public let atTopPercent: CGFloat
-    public let atBottomPercent: CGFloat
-    public let preferenceChangeTimeInterval: Int64
+    public var moreLoaderTreshold: CGFloat
+    public var reloaderTreshold: CGFloat
+    public var atTopPercent: CGFloat
+    public var atBottomPercent: CGFloat
+    public var preferenceChangeTimeInterval: Int64
     
     public static let defaultSettings = PaginatedScrollViewSettings(moreLoaderTreshold: 50, reloaderTreshold: 140, atTopPercent: 1.0, atBottomPercent: 0.01, preferenceChangeTimeInterval: 30)
 }
