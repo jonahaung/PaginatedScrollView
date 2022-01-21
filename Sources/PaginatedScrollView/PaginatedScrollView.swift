34988fa84e861733cae4adad921d179b1dfba119
Aung Ko Min <jonahaung@gmail.com>
2022-01-22 06:29:02 +0800

    3

--- Sources/PaginatedScrollView/PaginatedScrollView.swift
+++ Sources/PaginatedScrollView/PaginatedScrollView.swift
@@ -43,8 +43,7 @@ public struct PaginatedScrollView<Content: View>: View {
                 }
                 .padding(.bottom, geometry.safeAreaInsets.bottom)
                 .frame(width: geometry.size.width)
-                .background(Color(uiColor: .groupTableViewBackground).frame(maxHeight: .infinity))
-//                .background(Color(uiColor: .groupTableViewBackground).frame(height: 99999999))
+                .background(Color(uiColor: .groupTableViewBackground).frame(height: 99999999))
                 .anchorPreference(key: PaginatedScrollViewKey.PreKey.self, value: .bounds) {
                     guard canRefresh || canLoadMore && manager.canreturn else { return nil }
                     let frame = geometry[$0]
